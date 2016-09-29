dd0430a77e59ec1152bd4602b35cdfa8c0cc08d3
Sonny Tosco <sonny.tosco@gmail.com>
2016-09-29 08:54:58 -0700

    menu swipe working

---
+++ View2.swift
@@ -0,0 +1,39 @@
+//
+//  View2.swift
+//  snapchat
+//
+//  Created by Clavel Tosco on 9/29/16.
+//  Copyright © 2016 React. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+
+    @IBOutlet var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+    }
+
+}
--- snapchat/ViewController.swift
+++ snapchat/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var V1 : View1 = View1(nibName: "View1", bundle: nil)
+        var V2 : View2 = View2(nibName: "View2", bundle: nil)
+        var V3 : View3 = View3(nibName: "View3", bundle: nil)
+        
+        self.addChildViewController(V1)
+        self.scrollView.addSubview(V1.view)
+        V1.didMoveToParentViewController(self)
+        
+        self.addChildViewController(V2)
+        self.scrollView.addSubview(V2.view)
+        V2.didMoveToParentViewController(self)
+        
+        self.addChildViewController(V3)
+        self.scrollView.addSubview(V3.view)
+        V3.didMoveToParentViewController(self)
+        
+        var V2Frame : CGRect = V1.view.frame
+        V2Frame.origin.x = self.view.frame.width
+        V2.view.frame = V2Frame
+        
+        var V3Frame : CGRect = V3.view.frame
+        V3Frame.origin.x = 2 * self.view.frame.width
+        V3.view.frame = V3Frame
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.width * 3, self.view.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
